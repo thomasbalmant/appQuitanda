a90598652977d65a3a63f42d8c177fa93af3beff
thomasbalmant <thomas_dru@hotmail.com>
2022-10-17 15:01:09 -0300

    theme

--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -16,11 +16,27 @@ class SignInScreen extends StatelessWidget {
           ),
           Expanded(
             child: Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 32,
+                vertical: 40,
+              ),
               decoration: const BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.vertical(
                     top: Radius.circular(45),
                   )),
+              child: Column(
+                children: [
+                  //Email
+                  TextFormField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(18))),
+                  ),
+
+                  //Senha
+                ],
+              ),
             ),
           ),
         ],
