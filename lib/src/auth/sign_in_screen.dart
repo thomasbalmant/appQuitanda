ed439f71d95f48d9192edfc0ed09ff31197dbcfa
thomasbalmant <thomas_dru@hotmail.com>
2022-10-17 21:03:15 -0300

    logotipo

--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:quitanda_getx/src/components/custom_text_field.dart';
 
@@ -11,10 +12,55 @@ class SignInScreen extends StatelessWidget {
       body: Column(
         children: [
           Expanded(
-            child: Container(
-              color: Colors.red,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                //LOGOTIPO
+                const Text.rich(
+                  TextSpan(
+                      style: TextStyle(
+                        fontSize: 40,
+                      ),
+                      children: [
+                        TextSpan(
+                          text: 'Green',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        TextSpan(
+                          text: 'grocer',
+                          style: TextStyle(
+                            color: Colors.red,
+                          ),
+                        ),
+                      ]),
+                ),
+                //ANIMAÇÃO
+                SizedBox(
+                  height: 30,
+                  child: DefaultTextStyle(
+                    style: const TextStyle(
+                      fontSize: 25,
+                    ),
+                    child: AnimatedTextKit(
+                      repeatForever: true,
+                      animatedTexts: [
+                        FadeAnimatedText('Frutas'),
+                        FadeAnimatedText('Verduras'),
+                        FadeAnimatedText('Legumes'),
+                        FadeAnimatedText('Carnes'),
+                        FadeAnimatedText('Cereais'),
+                        FadeAnimatedText('Laticíneos'),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
+          //FORMULARIO
           Container(
             padding: const EdgeInsets.symmetric(
               horizontal: 32,
