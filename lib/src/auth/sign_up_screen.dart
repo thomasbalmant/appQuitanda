c0f01281eaec7ee2c7b55ad4a2509f6b4fac79d1
thomasbalmant <thomas_dru@hotmail.com>
2022-10-19 15:51:49 -0300

    inputs signup

--- lib/src/auth/sign_up_screen.dart
+++ lib/src/auth/sign_up_screen.dart
@@ -1,10 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:quitanda_getx/src/components/custom_text_field.dart';
+import 'package:quitanda_getx/src/config/custom_colors.dart';
 
 class SignUpScreen extends StatelessWidget {
   const SignUpScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: CustomColors.customSwatchColor,
+      body: Column(
+        children: [
+          const Expanded(
+            child: Center(
+              child: Text(
+                'Cadastro',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 35,
+                ),
+              ),
+            ),
+          ),
+
+          //FORUMULARIO
+          Container(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 32,
+              vertical: 40,
+            ),
+            decoration: const BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.vertical(
+                top: Radius.circular(45),
+              ),
+            ),
+            child: Column(
+              children: const [
+                CustomTextField(
+                  icon: Icons.email,
+                  label: 'Email',
+                ),
+                CustomTextField(
+                  icon: Icons.lock,
+                  label: 'Senha',
+                  isSecret: true,
+                ),
+                CustomTextField(
+                  icon: Icons.person,
+                  label: 'Nome',
+                ),
+                CustomTextField(
+                  icon: Icons.phone,
+                  label: 'Celular',
+                ),
+                CustomTextField(
+                  icon: Icons.file_copy,
+                  label: 'CPF',
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
