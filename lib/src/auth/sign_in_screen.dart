cc416c373b05ae2c043d4f16d35da9acc10acee2
thomasbalmant <thomas_dru@hotmail.com>
2022-10-19 15:40:50 -0300

    signup screen

--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -1,5 +1,6 @@
 import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
+import 'package:quitanda_getx/src/auth/sign_up_screen.dart';
 import 'package:quitanda_getx/src/components/custom_text_field.dart';
 import 'package:quitanda_getx/src/config/custom_colors.dart';
 
@@ -157,7 +158,13 @@ class SignInScreen extends StatelessWidget {
                     SizedBox(
                       height: 50,
                       child: OutlinedButton(
-                        onPressed: () {},
+                        onPressed: () {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(builder: (c) {
+                              return SignUpScreen();
+                            }),
+                          );
+                        },
                         // ignore: sort_child_properties_last
                         child: const Text(
                           'Criar Conta',
