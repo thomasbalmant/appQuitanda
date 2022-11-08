a5f69ab4b1bb82981a8134605b650c7f4f76960a
thomasbalmant <thomas_dru@hotmail.com>
2022-11-08 20:19:44 -0300

    page home

---
+++ lib/src/home/home_tab.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomeTab extends StatelessWidget {
+  const HomeTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+        // App bar
+
+        // Campo pesquisa
+
+        // Categorias
+
+        // Grid
+
+        );
+  }
+}
