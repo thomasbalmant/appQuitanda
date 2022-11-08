9c7f06b041b1d933cfb66672e8632b889e792580
thomasbalmant <thomas_dru@hotmail.com>
2022-11-08 20:27:50 -0300

    custom color

--- lib/src/home/home_tab.dart
+++ lib/src/home/home_tab.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../config/custom_colors.dart';
+
 class HomeTab extends StatelessWidget {
   const HomeTab({super.key});
 
@@ -8,14 +10,25 @@ class HomeTab extends StatelessWidget {
     return Scaffold(
       // App bar
       appBar: AppBar(
-        title: const Text.rich(
+        centerTitle: true,
+        title: Text.rich(
           TextSpan(
-            style: TextStyle(
+            style: const TextStyle(
               fontSize: 30,
             ),
             children: [
-              TextSpan(text: 'Green'),
-              TextSpan(text: 'grocer'),
+              TextSpan(
+                text: 'Green',
+                style: TextStyle(
+                  color: CustomColors.customSwatchColor,
+                ),
+              ),
+              TextSpan(
+                text: 'grocer',
+                style: TextStyle(
+                  color: CustomColors.customContrastColor,
+                ),
+              ),
             ],
           ),
         ),
