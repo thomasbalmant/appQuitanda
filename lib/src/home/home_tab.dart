5adc08783f736a6d5744dcaa8c30696d41b06075
thomasbalmant <thomas_dru@hotmail.com>
2022-11-08 20:47:14 -0300

    textformfield ok

--- lib/src/home/home_tab.dart
+++ lib/src/home/home_tab.dart
@@ -59,7 +59,41 @@ class HomeTab extends StatelessWidget {
         ],
       ),
 
-      // Campo pesquisa
+      body: Column(
+        children: [
+          // Campo pesquisa
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 10,
+            ),
+            child: TextFormField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                isDense: true,
+                hintText: 'Pesquise aqui...',
+                hintStyle: TextStyle(
+                  color: Colors.grey.shade400,
+                  fontSize: 14,
+                ),
+                prefixIcon: Icon(
+                  Icons.search,
+                  color: CustomColors.customContrastColor,
+                  size: 21,
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(60),
+                  borderSide: const BorderSide(
+                    width: 0,
+                    style: BorderStyle.none,
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
 
       // Categorias
 
