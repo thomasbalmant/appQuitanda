17cd42b67787c3f3f5eaa222c9f02016428e8476
thomasbalmant <thomas_dru@hotmail.com>
2022-11-08 20:29:35 -0300

    scaffoldbackgroundcolor

--- lib/main.dart
+++ lib/main.dart
@@ -15,8 +15,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.green,
-      ),
+          primarySwatch: Colors.green,
+          scaffoldBackgroundColor: Colors.white.withAlpha(190)),
       debugShowCheckedModeBanner: false,
       home: const SignInScreen(),
     );
