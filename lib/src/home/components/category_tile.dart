b4ed531446ff4342231bbab4ee62b5c3122bbda5
thomasbalmant <thomas_dru@hotmail.com>
2022-11-10 20:25:08 -0300

    category

--- lib/src/home/components/category_tile.dart
+++ lib/src/home/components/category_tile.dart
@@ -6,29 +6,36 @@ class CategoryTile extends StatelessWidget {
     super.key,
     required this.category,
     required this.isSelected,
+    required this.onPressed,
   });
 
   final String category;
   final bool isSelected;
+  final VoidCallback onPressed;
 
   @override
   Widget build(BuildContext context) {
-    return Align(
-      alignment: Alignment.center,
-      child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 6),
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10),
-          color:
-              isSelected ? CustomColors.customSwatchColor : Colors.transparent,
-        ),
-        child: Text(
-          category,
-          style: TextStyle(
-              color:
-                  isSelected ? Colors.white : CustomColors.customContrastColor,
-              fontWeight: FontWeight.bold,
-              fontSize: isSelected ? 16 : 14),
+    return GestureDetector(
+      onTap: onPressed,
+      child: Align(
+        alignment: Alignment.center,
+        child: Container(
+          padding: const EdgeInsets.symmetric(horizontal: 6),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            color: isSelected
+                ? CustomColors.customSwatchColor
+                : Colors.transparent,
+          ),
+          child: Text(
+            category,
+            style: TextStyle(
+                color: isSelected
+                    ? Colors.white
+                    : CustomColors.customContrastColor,
+                fontWeight: FontWeight.bold,
+                fontSize: isSelected ? 16 : 14),
+          ),
         ),
       ),
     );
