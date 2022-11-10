6160baa22a3a7e00a50d8602f0240cf5c30322b2
thomasbalmant <thomas_dru@hotmail.com>
2022-11-10 20:17:14 -0300

    hxaz1295

--- lib/src/home/components/category_tile.dart
+++ lib/src/home/components/category_tile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quitanda_getx/src/config/custom_colors.dart';
 
 class CategoryTile extends StatelessWidget {
   const CategoryTile({
@@ -12,6 +13,22 @@ class CategoryTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Align(
+      alignment: Alignment.center,
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 6),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(10),
+          color: CustomColors.customSwatchColor,
+        ),
+        child: Text(
+          category,
+          style: const TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
   }
 }
