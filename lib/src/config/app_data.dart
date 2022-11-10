6c7fd9dfcb930b68b4fb2377914850b92f2dc250
thomasbalmant <thomas_dru@hotmail.com>
2022-11-10 20:42:24 -0300

    assets

---
+++ lib/src/config/app_data.dart
@@ -0,0 +1,9 @@
+import 'package:quitanda_getx/src/models/item_model.dart';
+
+ItemModel apple = ItemModel(
+  description: description,
+  imgUrl: imgUrl,
+  itemName: itemName,
+  price: price,
+  unit: unit,
+);
