cee9bc9bab5361b3a748a981e8d64908ca7a4986
thomasbalmant <thomas_dru@hotmail.com>
2022-11-15 20:50:26 -0300

    item tile

--- lib/src/pages/home/components/item_tile.dart
+++ lib/src/pages/home/components/item_tile.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:quitanda_getx/src/models/item_model.dart';
 
 class ItemTile extends StatelessWidget {
-  const ItemTile({super.key});
+  final ItemModel item;
+
+  const ItemTile({super.key, required this.item});
 
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.blue);
+    return Card(
+      elevation: 5,
+      shadowColor: Colors.grey.shade300,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Column(
+        children: [
+          //IMAGE
+
+          //NOME
+
+          // PREÇO - UNIDADE
+        ],
+      ),
+    );
   }
 }
