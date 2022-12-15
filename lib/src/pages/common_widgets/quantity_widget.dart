cf7ca642f89d6fef360802d4369e89218732937e
thomasbalmant <thomas_dru@hotmail.com>
2022-12-15 10:46:06 -0300

    common components

---
+++ lib/src/pages/common_widgets/quantity_widget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class QuantityWidget extends StatelessWidget {
+  const QuantityWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
