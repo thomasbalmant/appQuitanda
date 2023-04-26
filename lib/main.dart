3b3d2950ad17c7a57b2974c0bd4ac42d1a9f9bb2
Thomas <thomas_dru@hotmail.com>
2023-04-26 15:10:39 -0400

    changes for the GetX

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:get/get.dart';
 import 'package:quitanda_getx/src/pages/splash/splash_screen.dart';
+import 'package:quitanda_getx/src/pages_routes/app_pages.dart';
 
 import 'src/pages/auth/sign_in_screen.dart';
 
@@ -31,13 +33,14 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Greengrocer',
       theme: ThemeData(
           primarySwatch: Colors.green,
           scaffoldBackgroundColor: Colors.white.withAlpha(190)),
       debugShowCheckedModeBanner: false,
-      home: const SplashScreen(),
+      initialRoute: PagesRoutes.splashRoute,
+      getPages: AppPages.pages,
     );
   }
 }
