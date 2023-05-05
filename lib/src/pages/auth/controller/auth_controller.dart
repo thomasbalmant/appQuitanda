e0b1d70146c8a4a8043ea3ae307c9a1507786d04
Thomas <thomas_dru@hotmail.com>
2023-05-05 16:33:09 -0400

    controller

--- lib/src/pages/auth/controller/auth_controller.dart
+++ lib/src/pages/auth/controller/auth_controller.dart
@@ -1,14 +1,17 @@
 import 'package:get/get.dart';
+import 'package:quitanda_getx/src/pages/auth/repository/auth_repository.dart';
 
 class AuthController extends GetxController {
   RxBool isLoading = false.obs;
+
+  final authRepository = AuthRepository();
 
   Future<void> Signin({
     required String email,
     required String password,
   }) async {
     isLoading.value = true;
-    await Future.delayed(const Duration(seconds: 2));
+    await authRepository.signIn(email: email, password: password);
     isLoading.value = false;
   }
 }
