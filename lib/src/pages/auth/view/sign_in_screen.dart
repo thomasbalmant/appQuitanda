c2c9085ace1c84a8f8184788a87dc495e54b2593
Thomas <thomas_dru@hotmail.com>
2023-05-08 11:26:43 -0400

    validators

--- lib/src/pages/auth/view/sign_in_screen.dart
+++ lib/src/pages/auth/view/sign_in_screen.dart
@@ -6,6 +6,7 @@ import 'package:quitanda_getx/src/pages/common_widgets/app_name_widget.dart';
 import 'package:quitanda_getx/src/pages/common_widgets/custom_text_field.dart';
 import 'package:quitanda_getx/src/config/custom_colors.dart';
 import 'package:quitanda_getx/src/pages_routes/app_pages.dart';
+import 'package:quitanda_getx/src/services/validators.dart';
 
 class SignInScreen extends StatelessWidget {
   SignInScreen({super.key});
@@ -82,15 +83,7 @@ class SignInScreen extends StatelessWidget {
                         controller: emailController,
                         icon: Icons.email,
                         label: 'Email',
-                        validator: (email) {
-                          if (email == null || email.isEmpty) {
-                            return 'Digite seu email!';
-                          }
-                          if (!email.isEmail) {
-                            return 'Digite um Email válido!';
-                          }
-                          return null;
-                        },
+                        validator: emailValidator,
                       ),
 
                       // SENHA
@@ -99,15 +92,7 @@ class SignInScreen extends StatelessWidget {
                         isSecret: true,
                         icon: Icons.lock,
                         label: 'Senha',
-                        validator: (password) {
-                          if (password == null || password.isEmpty) {
-                            return 'Digite sua senha';
-                          }
-                          if (password.length < 5) {
-                            return 'Digite uma senha com pelo menos 7 caracteres';
-                          }
-                          return null;
-                        },
+                        validator: passwordValidator,
                       ),
 
                       // BOTAO ENTRAR
